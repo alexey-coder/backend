c694c1ccfd00e1bf80b52b35deda907f79e6b5d9
Alexey <alex_coder@icloud.com>
2019-05-19 13:28:18 +0300

    fix

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -85,18 +85,7 @@ final class UsersController: RouteCollection {
     func getOneHandler(_ req: Request) throws -> Future<User> {
         return try req.parameters.next(User.self)
     }
-    
-//    func updatePasswordHandler(_ req: Request) throws -> Future<User.Public> {
-//        return try flatMap(to: User.Public.self, req.parameters.next(User.self), req.content.decode(User.self)) { (user, updatedUser) in
-//            let hasher = try req.make(BCryptDigest.self)
-//            let passwordHashed = try hasher.hash(updatedUser.password!)
-//            user.password = passwordHashed
-//            return user.save(on: req).map { storedUser in
-//                return User.Public(id: try storedUser.requireID(), email: storedUser.email)
-//            }
-//        }
-//    }
-    
+        
     func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
         return try req.parameters.next(User.self).flatMap { (user) in
             return user.delete(on: req).transform(to: HTTPStatus.noContent)
